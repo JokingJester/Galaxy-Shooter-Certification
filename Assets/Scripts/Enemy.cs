599419b9b6d1e600652b9449ad935b5bdd13f290
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-08-21 00:29:14 -0500

    Enemy Explodes When Hit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,17 +7,34 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _speed = 4;
     [SerializeField] private int _addedScore = 10;
     private Player _player;
+    private Animator _anim;
+    private BoxCollider2D _boxCollider2D;
+    private bool destroyEnemy;
 
 
     private void OnEnable()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
     }
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
         if(transform.position.y <= -5.4f)
             transform.position = new Vector3(Random.Range(-8, 8), 8, transform.position.z);
+
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Enemy_Destroyed_anim"))
+            destroyEnemy = true;
+        else
+        {
+            if (destroyEnemy == true)
+                Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,16 +42,20 @@ public class Enemy : MonoBehaviour
         if(other.tag == "Laser")
         {
             Destroy(other.gameObject);
-            _player.AddScore(_addedScore);
-            Destroy(this.gameObject);
+            if(_player != null)
+                _player.AddScore(_addedScore);
+            _anim.SetTrigger("OnEnemyDeath");
+            _boxCollider2D.enabled = false;
+            _speed = 0;
         }
 
         if(other.tag == "Player")
         {
-            Player player = other.GetComponent<Player>();
             if (_player != null)
                 _player.Damage();
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            _boxCollider2D.enabled = false;
+            _speed = 0;
         }
     }
 }
